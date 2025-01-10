d513117d7dd2063dd49864c0369847e193839dc1
diwebin-id <diwebinaja.company@gmail.com>
2025-01-11 01:23:56 +0700

    database-page #1

--- frontend/public/src/routes/index.js
+++ frontend/public/src/routes/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/about.vue";
 import Club from "../views/club.vue";
+import Database from "../views/database.vue";
 import Contact from "../views/contact.vue";
 import Events from "../views/events.vue";
 import Blog from "../views/blog.vue";
@@ -12,6 +13,7 @@ const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: About },
   { path: "/club", name: "Club", component: Club },
+  { path: "/database", name: "Database", component: Database },
   { path: "/contact", name: "Contac", component: Contact },
   { path: "/events", name: "Events", component: Events },
   { path: "/blog", name: "Blog", component: Blog },
---
+++ frontend/public/src/views/database.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <section
+      class="py-10 md:py-20 lg:py-32 xl:py-40 mx-10 lg:mx-32 xl:mx-56 lg:m-auto"
+    >
+      <div class="">
+        <h1
+          class="text-white uppercase text-xl lg:text-3xl font-bold tracking-wider"
+        >
+          Data Base
+        </h1>
+        <div class="w-[20%] h-1 bg-gray-200 mt-2"></div>
+        <hr />
+        <div>
+          <div class="flex justify-end">
+            <p
+              class="w-[90%] text-xs lg:text-base text-white text-justify pt-7"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              quibusdam facilis deleniti laudantium officiis asperiores,
+              molestiae non architecto eligendi veritatis ut nostrum dolores,
+              deserunt ullam numquam magni. Voluptatem, expedita quia.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- data-database -->
+      <div class="mt-20 border-white border-[1px]">
+        <div class="md:flex h-max">
+          <!-- Bagian Gambar -->
+          <div
+            class="w-full md:w-2/3 bg-gray-500 flex justify-center items-center overflow-hidden"
+          >
+            <img
+              src="../assets/images/74988-gedung-pemkab-jember.jpg"
+              alt="icon person"
+              class="w-full h-auto object-cover"
+            />
+          </div>
+
+          <div
+            class="bg-transparent text-white p-5 md:w-2/3 md:px-14 md:py-14 flex flex-col justify-center"
+          >
+            <h1 class="uppercase font-bold tracking-wider text-sm">
+              daftar pelatih
+            </h1>
+            <p class="font-normal text-xs pt-1 pb-2">2 Weeks</p>
+            <p class="font-normal text-xs pt-1 pb-2">2 Free</p>
+            <button
+              class="mt-2 uppercase bg-white font-bold w-[70%] rounded-sm md:w-[70%] text-xs px-3 py-1 tracking-widest text-black hover:shadow-[0_0_10px_4px_rgba(255,255,255,0.2)] hover:duration-300 duration-300"
+            >
+              View Details
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-20 border-white border-[1px]">
+        <div class="md:flex h-max">
+          <!-- Bagian Gambar -->
+          <div
+            class="w-full md:w-2/3 bg-gray-500 flex justify-center items-center overflow-hidden"
+          >
+            <img
+              src="../assets/images/74988-gedung-pemkab-jember.jpg"
+              alt="icon person"
+              class="w-full h-auto object-cover"
+            />
+          </div>
+
+          <div
+            class="bg-transparent text-white p-5 md:w-2/3 md:px-14 md:py-14 flex flex-col justify-center"
+          >
+            <h1 class="uppercase font-bold tracking-wider text-sm">
+              daftar pelatih
+            </h1>
+            <p class="font-normal text-xs pt-1 pb-2">2 Weeks</p>
+            <p class="font-normal text-xs pt-1 pb-2">2 Free</p>
+            <button
+              class="mt-2 uppercase bg-white font-bold w-[70%] rounded-sm md:w-[70%] text-xs px-3 py-1 tracking-widest text-black hover:shadow-[0_0_10px_4px_rgba(255,255,255,0.2)] hover:duration-300 duration-300"
+            >
+              View Details
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
